71f142f298ef31759e6f7366a4f71b0294cc6ddb
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-01-18 23:52:36 +0100

    HTML & CSS set, function names in JS set

--- main.js
+++ main.js
@@ -1,4 +1,54 @@
+function setKeys() {
+    // press W for wine
+    // use left/right arrow keys to move pillow
+}
 
+function setFallingObjects() {
+    // 
+}
+
+function meterAnimation() {
+    // set varying animation per color
+}
+
+function increaseMeter() {
+    // increases whenever a sheep or prop is caught
+}
+
+function drinkWine() {
+    // change color when clicked OR visibility set to HIDDEN
+}
+
+function addTime() {
+    // adds 5 secs after wine is clicked
+}
+
+function reduceMeter() {
+    // reduce meter by 2% every sec
+}
+
+function calcScore() {
+    // sheep = 5%
+    // sleep props = 10%
+    // hyper = 20%
+
+}
+
+function printMsg() {
+    // print different messages
+}
+
+function changeFace() {
+    // sleepy face when meter reaches green
+    // hyper face when red
+    // default happy face when blue
+
+}
+
+function winOrLose() {
+    // if score is in between 64%-82% (green), player wins
+    // print message if one wins or loses
+}
 
 // function initialize() {
 
